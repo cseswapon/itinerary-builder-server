3e9785a8768164059a007d45ce85337d22f5de87
cseswapon <cseswaponsaha@gmail.com>
2024-01-20 23:44:02 +0600

    [MOD] - modify some code

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const description = require('./routes/description.route');
-const itinerary = require("./routes/itinerary.route");
-const mongoose = require("mongoose");
-require("dotenv").config();
 require('colors')
 
 const port = process.env.PORT || 8080;
 
 
-// middleware
-app.use(express.json());
-app.use(cors());
-app.use("/api/v1/description",description)
-app.use("/api/v1/itinerary", itinerary);
 
 // public route test
 app.get("/", (req, res) => {
@@ -26,13 +16,6 @@ app.get("*", (req, res) => {
   res.send({ message: `This URL: ${req.url} is not found` });
 });
 
-// database connected
-mongoose
-  .connect(`${process.env.DATABASE_URI}/builder`)
-  .then(() => {
-    console.log(`Database Connected Successfully`.black.bgGreen);
-  })
-  .catch((err) => console.log(err.message).red.bold);
 
 // server running
 app.listen(port, () => {
