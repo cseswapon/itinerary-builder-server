d252b3f1f23024f448f93d6f2ce8d11b5299ef1d
cseswapon <cseswaponsaha@gmail.com>
2024-01-20 23:59:33 +0600

    [ADD] - added some change

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const description = require("./routes/description.route");
+const itinerary = require("./routes/itinerary.route");
+const mongoose = require("mongoose");
 require("dotenv").config();
-require('colors')
+require("colors");
 
 const port = process.env.PORT || 8080;
 
-
+// middleware
+app.use(express.json());
+app.use(cors());
+app.use("/api/v1/description", description);
+app.use("/api/v1/itinerary", itinerary);
 
 // public route test
 app.get("/", (req, res) => {
@@ -17,12 +25,20 @@ app.get("*", (req, res) => {
   res.send({ message: `This URL: ${req.url} is not found` });
 });
 
-
+// database connected
+mongoose
+  .connect(`${process.env.DATABASE_URI}/builder`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`Database Connected Successfully`.black.bgGreen);
+  })
+  .catch((err) => console.log(err.message).red.bold);
 
 // server running
 app.listen(port, () => {
   console.log(`Server is running port ${port}`.red.bold);
 });
-
 
 module.exports = app;
